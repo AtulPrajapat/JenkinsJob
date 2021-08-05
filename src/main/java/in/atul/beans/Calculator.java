cd4e5694aff70ca141cd3db7e7d5c7f53774f150
AtulPrajapat <atulprajapat77@gmail.com>
2021-08-05 17:03:45 +0530

    first commit

---
+++ src/main/java/in/atul/beans/Calculator.java
@@ -0,0 +1,13 @@
+package in.atul.beans;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+		int i=10;
+		int j=20;
+		
+		System.out.println("Result ::"+(i + j));
+	}
+
+}
